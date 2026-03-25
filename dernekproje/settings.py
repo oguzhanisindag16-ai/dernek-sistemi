e2c5f88e5463cee03be8f915e71f0da7d8082b9e
oguzhanisindag16-ai <oguzhanisindag16@gmail.com>
2026-03-25 20:04:37 +0300

    Update settings.py

--- dernekproje/settings.py
+++ dernekproje/settings.py
@@ -47,12 +47,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'dernekproje.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
-
 AUTH_PASSWORD_VALIDATORS = []
 
 LANGUAGE_CODE = 'tr-tr'

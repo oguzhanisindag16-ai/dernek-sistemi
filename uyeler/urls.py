c8aaacc6ebf9255831f221c3e25a30c55150bcb4
oguzhanisindag16-ai <oguzhanisindag16@gmail.com>
2026-03-25 21:51:29 +0300

    Update urls.py

--- uyeler/urls.py
+++ uyeler/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('uye/<int:pk>/duzenle/', views.uye_duzenle, name='uye_duzenle'),
     path('duyurular/', views.duyuru_listesi, name='duyuru_listesi'),
     path('duyuru-ekle/', views.duyuru_ekle, name='duyuru_ekle'),
-    path('giris/', auth_views.LoginView.as_view(template_name='uyeler/giris.html'), name='giris'),
+    path('giris/', auth_views.LoginView.as_view(template_name='uyeler/giris2.html'), name='giris'),
     path('cikis/', auth_views.LogoutView.as_view(), name='cikis'),
 ]
